caa00a123c1de9d45a5c816e8ee3dbcf3051ae45
abhijith <abhijith.v.m@lxisoft.com>
2020-03-05 11:36:06 +0530

    modification in userdetail

--- Appraisal/src/main/java/com/lxisoft/Appraisal/controller/AppraisalController.java
+++ Appraisal/src/main/java/com/lxisoft/Appraisal/controller/AppraisalController.java
@@ -24,75 +24,68 @@ public class AppraisalController {
 
 	@Autowired
 	private JPAService service;
-	
+
 	@RequestMapping("/")
-	public String home()
-	{
-		String timeStand =new SimpleDateFormat ("yyyy_MM_dd_HH_mm_ss").format( Calendar.getInstance().getTime());
+	public String home() {
+		String timeStand = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(Calendar.getInstance().getTime());
 		System.out.println(timeStand);
-		return  "adminLogin";
+		return "adminLogin";
 	}
+
 	@RequestMapping("/viewUsers")
-	public ModelAndView viewUsers()
-	{
-		ArrayList<Employee> employees=(ArrayList<Employee>) service.getAllUsers();
-		ModelAndView mv= new ModelAndView("viewAllUsers");
+	public ModelAndView viewUsers() {
+		ArrayList<Employee> employees = (ArrayList<Employee>) service.getAllUsers();
+		ModelAndView mv = new ModelAndView("viewAllUsers");
 		mv.addObject("list", employees);
 		return mv;
 	}
+
 	@RequestMapping("/addUser")
-	public String addUser()
-	{
-	
+	public String addUser() {
+
 		return "addUser";
 	}
+
 	@RequestMapping("/addU")
-	public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response)
-	{
-		
-		Employee employee= new Employee();
+	public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response) {
+
+		Employee employee = new Employee();
 		employee.setFirstName(request.getParameter("firstname"));
 		employee.setLastName(request.getParameter("lastname"));
 		employee.setEmailID(request.getParameter("email"));
 		employee.setCompany(request.getParameter("company"));
 		service.addUser(employee);
-		
-		ModelAndView mv=viewUsers();
-		
+
+		ModelAndView mv = viewUsers();
+
 		return mv;
 	}
 
 	@RequestMapping("/login")
-	public String loginPage()
-	{
-		return  "login";
+	public String loginPage() {
+		return "login";
 	}
+
 	@RequestMapping("/logout-success")
-	public String logoutPage()
-	{
-		return  "logout";
+	public String logoutPage() {
+		return "logout";
 
 	}
-	/*
-	 * @RequestMapping("/userDetails") public ModelAndView userDetail(@RequestParam
-	 * int id,ModelAndView model) { ArrayList<Employee>
-	 * employees=(ArrayList<Employee>) service.getAllUsers(); ModelAndView mv= new
-	 * ModelAndView("userDetail"); mv.addObject("id",id); for(int
-	 * i=0;i<employees.size();i++) { if(employees.get(i).id==id) {
-	 * mv.addObject("employee",employees.get(i)); } } return mv ; }
-	 */
-	
-	@RequestMapping("/userDetails")
-	public ModelAndView userDetail(HttpServletRequest request,HttpServletResponse response)
-	{
-		//ArrayList<Employee> employees=(ArrayList<Employee>) service.getAllUsers();
-		Employee employee =(Employee) request.getAttribute("employee");		
-		ModelAndView mv= new ModelAndView("userDetail");
-		mv.addObject("employee",employee);		
-		System.out.print(employee.firstName);
-		return mv ; 
-	}
 
+ @RequestMapping("/userDetails") 
+ public ModelAndView userDetail(@RequestParam int id,ModelAndView model) 
+ { ArrayList<Employee> employees=(ArrayList<Employee>) service.getAllUsers();
+ ModelAndView mv= new ModelAndView("userDetail"); 
+ mv.addObject("id",id); 
+ 
+ for(int i=0;i<employees.size();i++)
+ { 
+	 if(employees.get(i).id==id)
+	 {
+		 mv.addObject("employee",employees.get(i));
+	 }	 
+  } return mv ; 
+	 
+ }
+ 
 }
-
-
