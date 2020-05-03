96b33531b3b9707e5f7875519e98f5776f8899a9
ajithkp1537 <ajith.k.p@lxisoft.com>
2020-05-03 13:03:52 +0530

    invalid inputs

--- src/main/java/com/lxisoft/appraisal/web/ControllerResource.java
+++ src/main/java/com/lxisoft/appraisal/web/ControllerResource.java
@@ -954,7 +954,7 @@ public class ControllerResource {
 			appraisal=appraisalService.getOneAppraisal(id);
 		}catch(Exception e)
 		{
-			error();
+			error(id);
 		}
 		long attVal=appraisal.getAttendance();
 		long punVal=appraisal.getPunctuality();
@@ -983,9 +983,11 @@ public class ControllerResource {
 		ResponseEntity<byte[]> response=new ResponseEntity<byte[]>(pdfContents,headers,HttpStatus.OK);
 		return response;
 	}
-	public ModelAndView error() {
+	public ModelAndView error(long id) {
 		ModelAndView mv=new ModelAndView("redirect:/userDetails");
+		System.out.println("month ///////////////////////////////////////////");
 		mv.addObject("errorInApp", true);
+		mv.addObject("id",id);
 		return mv;
 	}
 	/**
@@ -1046,7 +1048,11 @@ public class ControllerResource {
 		 LocalDate first=LocalDate.parse(start);
 		 LocalDate second=LocalDate.parse(end);
 		 long days= ChronoUnit.DAYS.between(first,second);
-		 appraisalService.setAppraisalByDate(id, first, second);
+		 if(days>1)	 appraisalService.setAppraisalByDate(id, first, second);
+		 else {
+			 mv.addObject("errorInApp", true);
+			 error(id);
+		 }
 		 mv.addObject("randomApp",true);
 		 mv.addObject("id",id);
 		 mv.addObject("astart",start.toString());
@@ -1058,7 +1064,7 @@ public class ControllerResource {
 	public ModelAndView pdfByMonth(@RequestParam Long id,@RequestParam (name="month") String month)
 	{
 		ModelAndView mv= new ModelAndView("redirect:/userDetails"); 	
-		System.out.println("month "+month);
+		
 		String[] values = month.split("-");
 		Calendar calendar = Calendar.getInstance();
 		int year = Integer.parseInt(values[0]);
@@ -1101,6 +1107,16 @@ public class ControllerResource {
 		 ModelAndView mv= new ModelAndView("userDetail"); 	
 		 LocalDate start1=LocalDate.parse(start);
 		 LocalDate end1=LocalDate.parse(end);		
+		 
+		 long daysBetween= ChronoUnit.DAYS.between(start1,end1);
+		 if(daysBetween<2)
+		 {
+			 mv= new ModelAndView("redirect:/userDetails"); 	
+			 mv.addObject("errorInApp",true);
+			 mv.addObject("id",id);
+			 return mv;
+		 }
+		 
 		 
 		 Optional <User> user = userService.findByid(id);
 		 Optional <UserExtra> userEx = userService.findExtraByid(id);
@@ -1170,10 +1186,9 @@ public class ControllerResource {
 		 }
 		
 		  UserExtraDTO dto=getUser(user.get(),userEx.get());
-		  mv.addObject("employee",dto);		
-		
-		 long days= ChronoUnit.DAYS.between(start1,end1);
-		 long total=(days*7);
+		  mv.addObject("employee",dto);	
+		  long days= ChronoUnit.DAYS.between(start1,end1);
+		  long total=(days*7);
 		
 		 List<LateArrival> a=new ArrayList<LateArrival>();
 		 List<LateArrival> un=new ArrayList<LateArrival>();
