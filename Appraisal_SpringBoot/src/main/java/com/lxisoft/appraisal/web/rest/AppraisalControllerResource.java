c0938f00b006359dd7cb816fa62d4b872f2eacbb
meharunnisamk <meharunnisamk@github.com>
2020-06-06 07:35:48 +0530

    user full work profie

--- Appraisal_SpringBoot/src/main/java/com/lxisoft/appraisal/web/rest/AppraisalControllerResource.java
+++ Appraisal_SpringBoot/src/main/java/com/lxisoft/appraisal/web/rest/AppraisalControllerResource.java
@@ -19,11 +19,17 @@ import org.springframework.transaction.annotation.Transactional;
 import com.lxisoft.appraisal.config.Constants;
 import com.lxisoft.appraisal.domain.UserExtra;
 import com.lxisoft.appraisal.repository.UserExtraRepository;
+import com.lxisoft.appraisal.domain.Git;
+import com.lxisoft.appraisal.domain.Hackathon;
 import com.lxisoft.appraisal.domain.LateArrival;
 import com.lxisoft.appraisal.domain.Leave;
+import com.lxisoft.appraisal.domain.ReportStatus;
 import com.lxisoft.appraisal.domain.User;
+import com.lxisoft.appraisal.service.GitService;
+import com.lxisoft.appraisal.service.HackathonService;
 import com.lxisoft.appraisal.service.LateArrivalService;
 import com.lxisoft.appraisal.service.LeaveService;
+import com.lxisoft.appraisal.service.ReportStatusService;
 import com.lxisoft.appraisal.service.RestService;
 import com.lxisoft.appraisal.service.UserExtraService;
 import com.lxisoft.appraisal.service.UserService;
@@ -61,6 +67,12 @@ public class AppraisalControllerResource {
 	LeaveService leaveSer;
 	@Autowired
 	LateArrivalService lateServ;
+	@Autowired
+	ReportStatusService reportServ;
+	@Autowired
+	GitService gitServ;
+	@Autowired
+	HackathonService hackServ;
 
 
     private final Logger log = LoggerFactory.getLogger(AppraisalControllerResource.class);
@@ -111,23 +123,19 @@ public class AppraisalControllerResource {
        // restService.setLeave(userDTO);
     	return userRes.getAllUsers(pageable);
     }
+    /**
+     * for getting leaves and late arrival status
+     * @param id- id of user
+     * @return 
+     */
     @GetMapping("/status/{id}")
     public List<Integer> getUserStatus(@PathVariable Long id)
     {
     	Optional <User> user = userexService.findByid(id);
         log.debug("REST request to get UserExtra for status : {}", id);
-        Optional<UserExtra> userExtra = userExtraRepository.findById(id);
-        log.debug("REST  get UserExtra  for status: {}", userExtra);
         List<Leave> leave = leaveSer.findLeave(id);
         List<Integer> number=new ArrayList<Integer>();
 		 List<LateArrival> late =lateServ.findLate(id);
-		 List<LocalDateTime> time=new ArrayList<LocalDateTime>();
-		 for(int i=0;i<late.size();i++)
-		 {
-			 Instant in=late.get(i).getReachedTime();
-			 LocalDateTime t= LocalDateTime.ofInstant(in,ZoneId.systemDefault());
-			 time.add(t);
-		 }
 		 List<Leave> auth=new ArrayList<Leave>();
 		 List<Leave> unauth=new ArrayList<Leave>();
 		 for(int i=0;i<leave.size();i++)
@@ -157,13 +165,66 @@ public class AppraisalControllerResource {
 			 }
 		 }
 		 number.add(a.size());
-		 number.add(un.size());
-//		 Integer[] num=new Integer[4];
-//		 number.toArray(num);
-//		 
-        return number;
+		 number.add(un.size());	 
+		 List<Integer> num=getUserWorkingStatus(id,auth,unauth,number);
+		 List<Integer> value=getMarks(id,num);
+        return value;
     }
 
+    public List<Integer> getUserWorkingStatus(Long id,List<Leave> auth,List<Leave> unauth,List<Integer> number)
+    {
+    	Optional <UserExtra> userEx = userexService.findExtraByid(id);
+    	 List<ReportStatus> status=reportServ.findReport(id);
+		 List<ReportStatus> unreportdays=new ArrayList<ReportStatus>();
+		 for(int i=0;i<status.size();i++)
+		 {
+			unreportdays.add(status.get(i));
+		 }
+		 LocalDate first=userEx.get().getJoiningDate();
+		 LocalDate second= LocalDate.now();
+		 long workingDays= ChronoUnit.DAYS.between(first,second);
+		 int workingHour=(int)(workingDays*7);
+		 int leaveSize=((auth.size())+(unauth.size()));
+		 int absence=leaveSize*7;
+		 int workedHour=(workingHour-absence);
+		 int workedDays=((int)workingDays)-leaveSize;
+		 number.add((int)workingDays);
+		 number.add(workedDays);
+		 number.add(workingHour);
+		 number.add(workedHour);
+		 number.add(leaveSize);
+		 number.add(unreportdays.size());
+		 return number;
+    }
+    public List<Integer> getMarks(Long id,List<Integer> num)
+    {
+    	 List<Git> git=gitServ.findGit(userexService.findExtraByid(id).get().getId());
+		 List<Hackathon> hack=hackServ.findHack(userexService.findExtraByid(id).get().getId());
+		 if(git.size()!=0) 
+		 {
+			Iterator it=git.iterator();
+			while (it.hasNext())
+			{
+				Git object = (Git)it.next();
+				long gits= object.getMark();
+				num.add((int)gits);
+			 }	
+		}	
+		else { num.add(git.size());}
+		if(hack.size()!=0)
+		{
+			Iterator i=hack.iterator();
+			while (i.hasNext())
+			{
+				Hackathon object = (Hackathon)i.next();
+				long hacks= object.getMark();
+				num.add((int)hacks);
+			 }	
+		}
+		else { num.add(git.size());}
+		return num;
+		
+    }
 
 
 }
