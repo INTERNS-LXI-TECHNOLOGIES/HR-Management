fe95b95c4653f8f86d79b07a63349edb064894a4
meharunnisamk <meharunnisamk@lxisoft.com>
2020-06-22 22:52:53 +0530

    redirecting to user and admin page

--- Appraisal_SpringBoot/src/main/java/com/lxisoft/appraisal/config/SecurityConfiguration.java
+++ Appraisal_SpringBoot/src/main/java/com/lxisoft/appraisal/config/SecurityConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
-import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.web.filter.CorsFilter;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
@@ -65,10 +64,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
             .headers()
             .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:")
-        http.headers()
-            .addHeaderWriter(
-                    new StaticHeadersWriter("Access-Control-Allow-Origin", "address for your front-end here")
-            );
         .and()
             .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
         .and()
