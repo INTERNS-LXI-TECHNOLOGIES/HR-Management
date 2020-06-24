0a1ca2b66cb02e01f12f2db04447cb84d0e10ac0
meharunnisamk <meharunnisa.m.k@lxisoft.com>
2020-06-24 18:56:18 +0530

    alert setting

--- Angular_ionic/src/app/app-routing.module.ts
+++ Angular_ionic/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './guard/auth.guard';
 
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
@@ -5,8 +6,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {    path: '', redirectTo: 'login', pathMatch: 'full'  },
   {    path: 'login', loadChildren: () => import('./Pages/login/login.module').then( m => m.LoginPageModule) },
-  {    path: 'menu',  loadChildren: () => import('./Pages/menu/menu.module').then( m => m.MenuPageModule)  },
-  {    path: 'user-info', loadChildren: () => import('./Pages/user-info/user-info.module').then( m => m.UserInfoPageModule) },
+  {
+    path: 'menu',  loadChildren: () => import('./Pages/menu/menu.module').then( m => m.MenuPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'user-info', loadChildren: () => import('./Pages/user-info/user-info.module').then( m => m.UserInfoPageModule),
+    canActivate: [AuthGuard]
+  },
 
 ];
 @NgModule({
